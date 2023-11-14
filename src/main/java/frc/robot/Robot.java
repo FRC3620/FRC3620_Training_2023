b42a0e47fc362ef28a45912d56e4697b85717529
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-11-14 15:36:27 -0500

    Added a message for class.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -99,6 +99,8 @@ public class Robot extends TimedRobot {
     }
 
     processRobotModeChange(RobotMode.TELEOP);
+
+    logger.info ("Hi!");
   }
 
   /** This function is called periodically during operator control. */
