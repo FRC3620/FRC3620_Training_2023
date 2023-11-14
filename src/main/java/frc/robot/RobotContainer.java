f4eac32b9236e203ee9568e5d250f84d90d87fbf
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-11-14 14:58:00 -0500

    Get rid of cruft.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,19 +4,14 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import java.util.Set;
 import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.LogCommand;
 import org.usfirst.frc3620.logger.EventLogging.Level;
-import org.usfirst.frc3620.misc.CANDeviceFinder;
 
 import edu.wpi.first.wpilibj2.command.Command;
-
-import org.usfirst.frc3620.misc.DPad;
-import org.usfirst.frc3620.misc.RobotParametersContainer;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,10 +22,6 @@ import org.usfirst.frc3620.misc.RobotParametersContainer;
 public class RobotContainer {
   public final static Logger logger = EventLogging.getLogger(RobotContainer.class, Level.INFO);
   
-  // need this
-  public static CANDeviceFinder canDeviceFinder;
-  public static RobotParameters robotParameters;
-
   // hardware here...
 
   // subsystems here
@@ -41,22 +32,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    canDeviceFinder = new CANDeviceFinder();
-
-    robotParameters = RobotParametersContainer.getRobotParameters(RobotParameters.class);
-    logger.info ("got parameters for chassis '{}'", robotParameters.getName());
-
     makeSubsystems();
-
-    // CAN bus ok?
-    Set<CANDeviceFinder.NamedCANDevice> missingDevices = canDeviceFinder.getMissingDeviceSet();
-    if (missingDevices.size() > 0) {
-      SmartDashboard.putBoolean("can.ok", false);
-      SmartDashboard.putString("can.missing", missingDevices.toString());
-    } else {
-      SmartDashboard.putBoolean("can.ok", true);
-      SmartDashboard.putString("can.missing", "");
-    }
 
     // Configure the button bindings
     configureButtonBindings();
@@ -79,7 +55,7 @@ public class RobotContainer {
     driverJoystick = new Joystick(0);
     operatorJoystick = new Joystick(1);
 
-    DPad operatorDPad = new DPad(operatorJoystick, 0);
+    // DPad operatorDPad = new DPad(operatorJoystick, 0);
   }
 
   private void setupSmartDashboardCommands() {
@@ -104,33 +80,5 @@ public class RobotContainer {
     Command command = factory.get();
     logger.info ("Command Factory gave us a {}", command);
     return command;
-  }
-
-  /**
-   * Determine if we should make software objects, even if the device does 
-   * not appear on the CAN bus.
-   *
-   * We should if it's connected to an FMS.
-   *
-   * We should if it is missing a grounding jumper on DigitalInput 0.
-   *
-   * We should if the robot_parameters.json says so for this MAC address.
-   * 
-   * @return true if we should make all software objects for CAN devices
-   */
-  public static boolean shouldMakeAllCANDevices() {
-    if (DriverStation.isFMSAttached()) {
-      return true;
-    }
-
-    if (!Robot.isReal()) {
-      return false;
-    }
-
-    if (robotParameters.shouldMakeAllCANDevices()) {
-      return true;
-    }
-
-    return false;
   }
 }
